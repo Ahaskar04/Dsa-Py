59c649f98c345c89e6b882490447b31f0b0b7b3f
demo <kashyapahaskar@gmail.com>
2025-03-12 15:57:03 +0800

    Updated Dictionary

--- 2. Dictionary /1. Dictionary.py
+++ 2. Dictionary /1. Dictionary.py
@@ -33,3 +33,23 @@ my_dict().pop('address', None)
 print(my_dict)
 my_dict.clear()
 print(my_dict)
+
+#Dictionary Methods
+my_dict = {'name': 'Jack', 'age': 26, 'address': 'Downtown', 'education': 'Engineering'}
+dict = my_dict.copy() #returns a copy of the dictionary
+my_dict.get('name', None) #returns the value of the specified key
+my_dict.items() #returns a list of key-value pairs
+my_dict.keys() #returns a list of keys
+my_dict.values() #returns a list of values
+my_dict.popitem() #removes the last key-value pair
+my_dict.setdefault('address', None) #returns the value of the specified key. If the key does not exist: insert the key, with the specified value
+
+new_dict = {'name': 'Jill', 'age': 23}
+my_dict.update(new_dict)
+
+sorted(new_dict)
+
+#Dictionary Comprehension
+# new_dict = {new_key: new_value for item in list}
+new_dict = {new_key: new_value for new_key, new_value in dict.items()}
+new_dict = {new_key: new_value for new_key, new_value in dict.items() if new_key != 'age'}
