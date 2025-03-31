c123af356c5a765724ace373c2f95e334ff216bc
demo <kashyapahaskar@gmail.com>
2025-03-31 22:28:56 +0800

    better leetcode q5 ll

--- 5. Linked List/LeetCode/q5.py
+++ 5. Linked List/LeetCode/q5.py
@@ -20,4 +20,37 @@ class Solution(object):
         else:
             return False
             
+# This is not the best solution as the same complexity can be achieved with O(n) time and O(1) space.
+# The above solution has O(n) time and O(n) space complexity.
+
+
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+ 
+class Solution(object):
+    def isPalindrome(self, head):
+        #find te middle of the linked list
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
         
+        #reverse the second half of the linked list
+        prev = None
+        while slow:
+            nxt = slow.next
+            slow.next = prev
+            prev = slow
+            slow = nxt
+            
+        #compare the first half and the reversed second half
+        #head is the first half and prev is the reversed second half
+        while prev:
+            if head.val != prev.val:
+                return False
+            head = head.next
+            prev = prev.next
+        return True
+    
