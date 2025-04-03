651e633672c5073c4e4fdce87435c7f8a0ed2a7f
demo <kashyapahaskar@gmail.com>
2025-04-04 02:04:40 +0800

    q1 Cll

---
+++ 6. Circular Singly LL/q1.py
@@ -0,0 +1,52 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+    
+    def __str__(self):
+        return str(self.value)
+
+class CSLinkedList:
+    
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+    
+    def __str__(self):
+        temp_node = self.head
+        result = ''
+        while temp_node is not None:
+            result += str(temp_node.value)
+            temp_node = temp_node.next
+            if temp_node == self.head:  # Stop condition for circular list
+                break
+            result += ' -> '
+        return result
+    
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            new_node.next = new_node  # Point to itself to form a circle
+        else:
+            new_node.next = self.head
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            new_node.next = new_node
+        else:
+            new_node.next = self.head
+            self.tail.next = new_node
+            self.head = new_node
+        self.length += 1
+
+
+
