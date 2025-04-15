1b9d6d202f1b95f8c5f620bd7c964a5c13c4374c
demo <kashyapahaskar@gmail.com>
2025-04-16 01:29:59 +0800

    Circular doubly linked list

---
+++ 8. Circular Doubly Linked List/1. Cdll.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+    def __str__(self):
+        return str(self.data)
+
+class CircularDoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def __str__(self):
+        temp_node = self.head
+        result = ''
+        if temp_node is not None:
+            while True:
+                result += str(temp_node.data)
+                if temp_node.next == self.head:
+                    break
+                result += ' <-> '
+                temp_node = temp_node.next
+        return result
+    
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            new_node.next = new_node
+            new_node.prev = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.tail = new_node
+        self.length += 1
